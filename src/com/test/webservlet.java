67a9c84e7745444e1a612bb6d8c565847aca07db
pangjlin <1508332071@qq.com>
2015-12-17 10:44:44 +0800

    5 commit

---
+++ src/com/test/webservlet.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class webservlet
+ */
+@WebServlet("/webservlet")
+public class webservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	public String getData(){  
+        StringBuffer sb = new StringBuffer();  
+        sb.append("Hello~First Servlet!中文");  
+        return sb.toString();  
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String data = this.getData();  
+        response.setContentType("text/html;charset=UTF-8");  
+        response.getOutputStream().write(data.getBytes("UTF-8"));
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
